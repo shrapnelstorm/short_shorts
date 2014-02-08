4484705e0e279cc27c83be4178034c06667e515a
sivanr <sivanr@cs>
2014-02-08 14:04:05 -0800

    function to compute majority in an array in linear time implemented

---
+++ countvotes.py
@@ -0,0 +1,20 @@
+def majority(array,maj_threshold):
+    counter = 1
+    guess = array[]
+    for i in range(1,array.amount()):
+        if array[i] == guess :
+            counter++
+        else:
+            if counter == 0:
+                counter = 1
+                guess = array[i]
+            else: 
+                counter--
+    counter = 0
+    for i in range(array.amount()):
+        if array[i]==guess:
+            counter++
+    if counter >= maj_threshold :
+        return [1,guess]
+    else:
+        return [0,guess]
